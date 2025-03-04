630d9c9aed7f04397ac344a8201845d3e1ce4614
sirkarun <sirkarun@gmail.com>
2025-03-04 14:01:04 +0700

    fix for githubpage

--- src/App.js
+++ src/App.js
@@ -1,47 +1,38 @@
-
-import './App.css';
-import Navbar from './Navbar';
-import {BrowserRouter as Router,Route} from 'react-router-dom'
-import Home from './home';
-import About from './About';
-import Education from './Education';
-import Skill from './Skill';
-import Contact from './Contact';
-import Workexp from './Workexp';
+import "./App.css";
+import Navbar from "./Navbar";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "./home";
+import About from "./About";
+import Education from "./Education";
+import Skill from "./Skill";
+import Contact from "./Contact";
+import Workexp from "./Workexp";
 function App() {
   return (
     <Router>
-    <div className="App">
-    
-      <Navbar/>
-      
-      <Route exact path="/">
-        <Home/>
-      </Route>
-      <Route exact path="/About">
-        <About/>
-      </Route>
-      <Route exact path="/Workexp">
-        <Workexp/>
-      </Route>
-      <Route exact path="/education">
-        <Education/>
-      </Route>
-      <Route exact path="/skill">
-        <Skill/>
-      </Route>
-      <Route exact path="/contact">
-        <Contact/>
-      </Route>
+      <div className="App">
+        <Navbar />
 
-
-      
-      
-      
-      
-    </div>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/About">
+          <About />
+        </Route>
+        <Route exact path="/Workexp">
+          <Workexp />
+        </Route>
+        <Route exact path="/education">
+          <Education />
+        </Route>
+        <Route exact path="/skill">
+          <Skill />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+      </div>
     </Router>
-    
   );
 }
 
